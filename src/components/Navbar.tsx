212819239006ad0de81dc50b9ad1be26335b665b
Juan Buitrago <buitr4go@gmail.com>
2023-08-29 00:47:12 -0700

    general frontend setup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
                 <img src={icon}
                     className="md:w-[50px] md:h-[65px] w-[40px] h-[50px]"
                     alt="" />
-                <h1 className="text-gray-50 ml-2 font-bold md:text-lg text-md">Ti<span className="text-[#5684db]">k</span>t<span className="text-[#ff76c8]">o</span>k<br/> W<span className="text-[#ff76c8]">i</span>pe L<span className="text-[#ff76c8]">o</span><span className="text-[#5684db]">a</span>d</h1>
+                <h1 className="text-gray-50 ml-2 font-bold md:text-lg text-md">Ti<span className="text-[#6599fb]">k</span>t<span className="text-[#ff96d5]">o</span>k<br/> W<span className="text-[#ff96d5]">i</span>pe L<span className="text-[#ff96d5]">o</span><span className="text-[#5684db]">a</span>d</h1>
             </div>
         </div>
     )
